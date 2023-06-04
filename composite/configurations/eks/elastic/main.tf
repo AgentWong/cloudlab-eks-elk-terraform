321662a3fda315e23317762bcd3c0bf03fab16d7
Herman Wong <3461848+AgentWong@users.noreply.github.com>
2023-06-04 10:17:45 -1000

    Add eck

---
+++ composite/configurations/eks/elastic/main.tf
@@ -0,0 +1,33 @@
+provider "kubernetes" {
+  host                   = varcluster_endpoint
+  cluster_ca_certificate = base64decode(var.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    # This requires the awscli to be installed locally where Terraform is executed
+    args = ["eks", "get-token", "--cluster-name", var.cluster_name]
+  }
+}
+
+resource "kubernetes_manifest" "elastic_crds" {
+  manifest = yamldecode(file("${path.module}/templates/crds.yaml"))
+}
+
+resource "kubernetes_manifest" "elastic_operator" {
+  manifest = yamldecode(file("${path.module}/templates/operator.yaml"))
+
+  depends_on = [ kubernetes_manifest.elastic_crds ]
+}
+
+resource "time_sleep" "elastic_operator" {
+    depends_on = [ kubernetes_manifest.elastic_operator ]
+
+    create_duration = "60s"
+}
+
+resource "kubernetes_manifest" "elasticsearch" {
+  manifest = yamldecode(file("${path.module}/templates/elasticsearch.yaml"))
+
+  depends_on = [ time_sleep.elastic_operator ]
+}
---
+++ composite/configurations/eks/elastic/variables.tf
@@ -0,0 +1,9 @@
+variable "cluster_endpoint" {
+    type = string
+}
+variable "cluster_certificate_authority_data" {
+    type = string
+}
+variable "cluster_name" {
+    type = string
+}
