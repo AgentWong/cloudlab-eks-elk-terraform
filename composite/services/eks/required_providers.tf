f60932541ed696652935352c355a912885c3ad07
Herman Wong <3461848+AgentWong@users.noreply.github.com>
2023-06-06 14:41:24 -1000

    Update providers.

--- composite/services/eks/required_providers.tf
+++ composite/services/eks/required_providers.tf
@@ -1,5 +1,11 @@
 terraform {
+    required_version = "~> 1.4.0"
+  
   required_providers {
+    aws = {
+        source  = "hashicorp/aws"
+        version = ">= 4.21"
+      }
     utils = {
       source = "cloudposse/utils"
       version = "1.8.0"
